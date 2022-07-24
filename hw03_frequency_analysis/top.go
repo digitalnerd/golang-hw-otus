e575dd66865037bb4647f5cf36a68e098d9a8a7c
digitalnerd <levinkv.work@gmail.com>
2022-07-25 00:03:47 +0300

    Added implementation of removing duplicate elements

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	// "fmt"
 	"fmt"
-	"reflect"
 	"strings"
 )
-
+// look at this example -> https://www.geeksforgeeks.org/how-to-remove-duplicate-values-from-slice-in-golang/
 func Top10(str string) []string {
-	fmt.Println(str)
+	// fmt.Println(str)
 
 	words := strings.Fields(str)
-	topWords := make(map[int]string)
-	fmt.Println(reflect.TypeOf(words))
-	for i, word := range words {
-		topWords[i] = word
+	topWords := make(map[string]bool)
+	list := []string{}
+
+	for _, entry := range words {
+		if _, value := topWords[entry]; !value {
+			topWords[entry] = true
+			list = append(list, entry)
+		}
 	}
 
-	for i, key := range topWords {
-		fmt.Println(i, key)
-	}
+	// fmt.Println(reflect.TypeOf(words))
 
-	return nil
+	// for i, word := range words {
+	// 	topWords[word] = i
+	// }
+
+	// for i, key := range topWords {
+	// 	fmt.Println(i, key)
+	// }
+	// fmt.Println(topWords)
+
+	// for i, word := range words {
+	// 	// _, ok := topWords[word]
+	// 	if _, ok := topWords[i]; ok {
+	// 		// fmt.Println(v)
+	// 	}
+	// }
+
+	return list
 }
 
 func main() {
 	str := "one two three one two four five six five"
-	Top10(str)
+	fmt.Println(Top10(str))
 }
