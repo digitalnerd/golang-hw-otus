ed15aa70f143f04e7b935f9ed6f68109d0e1b7d8
digitalnerd <levinkv.work@gmail.com>
2022-07-24 22:58:03 +0300

    Created a map

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	// "reflect"
 	"strings"
 )
 
@@ -9,8 +10,17 @@ func Top10(str string) []string {
 	fmt.Println(str)
 
 	words := strings.Fields(str)
-	for i, w := range words {
-		fmt.Println(i, w)
+	topWords := make(map[int]string)
+	// fmt.Println(reflect.TypeOf(words))
+	for i, word := range words {
+		// fmt.Println(word)
+		// put words to a map
+		// topWords = make(map[int]string)
+		topWords[i] = word
+		for i, key := range topWords {
+			fmt.Println(i, key)
+		}
+
 	}
 	return nil
 }
