6117a11ddf1dda9f60dc24c7ab409b0e4af6b676
digitalnerd <levinkv.work@gmail.com>
2022-07-24 22:18:11 +0300

    Added something...

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,9 +1,21 @@
-package hw03frequencyanalysis
+package main
 
-func Top10(_ string) []string {
-	
+import (
+	"fmt"
+	"strings"
+)
 
+func Top10(str string) []string {
+	fmt.Println(str)
 
-
+	words := strings.Fields(str)
+	for i, w := range words {
+		fmt.Println(i, w)
+	}
 	return nil
+}
+
+func main() {
+	str := "one two three one two four five six five"
+	Top10(str)
 }
