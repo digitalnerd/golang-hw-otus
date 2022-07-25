aa5d1ec69ab0b5b63dae5b3fd40631c2c5156cf0
digitalnerd <levinkv.work@gmail.com>
2022-07-25 23:47:42 +0300

    It's working but something somewhere is wrong... The tests are not passing.

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	// "fmt"
 	"fmt"
 	"sort"
 	"strings"
 )
 
 // this one was very helpful -> https://www.geeksforgeeks.org/how-to-sort-golang-map-by-keys-or-values/
-func Top10(str string) {
+func Top10(str string) []string {
 
 	words := strings.Fields(str)
 	topWords := make(map[string]int)
@@ -20,8 +21,6 @@ func Top10(str string) {
 		}
 	}
 
-	fmt.Println(topWords)
-
 	keys := make([]string, 0, len(topWords))
 	for key := range topWords {
 		keys = append(keys, key)
@@ -31,12 +30,52 @@ func Top10(str string) {
 		return topWords[keys[i]] > topWords[keys[j]]
 	})
 
-	for _, k := range keys{
-        fmt.Println(k, topWords[k])
-    }
+	// show how the map is look like
+	// for _, k := range keys {
+	// 	fmt.Println(k, topWords[k])
+	// }
+
+	// topTenWordsInTheList := make([]string, 0, len(keys[:10]))
+	var topTenWordsInTheList []string
+	topTenWordsInTheList = keys[:10]
+	return topTenWordsInTheList
 }
 
 func main() {
-	str := "one one one one one one one one two two three four five five five six six six six six"
-	Top10(str)
+	// str := "one one one one one one one one two two two two two two three four five five five six six six six six seven nine ten ten eleven eleven eleven twelve"
+	var str = `Как видите, он  спускается  по  лестнице  вслед  за  своим
+	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
+	ступеньки собственным затылком:  бум-бум-бум.  Другого  способа
+	сходить  с  лестницы  он  пока  не  знает.  Иногда ему, правда,
+		кажется, что можно бы найти какой-то другой способ, если бы  он
+	только   мог   на  минутку  перестать  бумкать  и  как  следует
+	сосредоточиться. Но увы - сосредоточиться-то ему и некогда.
+		Как бы то ни было, вот он уже спустился  и  готов  с  вами
+	познакомиться.
+	- Винни-Пух. Очень приятно!
+		Вас,  вероятно,  удивляет, почему его так странно зовут, а
+	если вы знаете английский, то вы удивитесь еще больше.
+		Это необыкновенное имя подарил ему Кристофер  Робин.  Надо
+	вам  сказать,  что  когда-то Кристофер Робин был знаком с одним
+	лебедем на пруду, которого он звал Пухом. Для лебедя  это  было
+	очень   подходящее  имя,  потому  что  если  ты  зовешь  лебедя
+	громко: "Пу-ух! Пу-ух!"- а он  не  откликается,  то  ты  всегда
+	можешь  сделать вид, что ты просто понарошку стрелял; а если ты
+	звал его тихо, то все подумают, что ты  просто  подул  себе  на
+	нос.  Лебедь  потом  куда-то делся, а имя осталось, и Кристофер
+	Робин решил отдать его своему медвежонку, чтобы оно не  пропало
+	зря.
+		А  Винни - так звали самую лучшую, самую добрую медведицу
+	в  зоологическом  саду,  которую  очень-очень  любил  Кристофер
+	Робин.  А  она  очень-очень  любила  его. Ее ли назвали Винни в
+	честь Пуха, или Пуха назвали в ее честь - теперь уже никто  не
+	знает,  даже папа Кристофера Робина. Когда-то он знал, а теперь
+	забыл.
+		Словом, теперь мишку зовут Винни-Пух, и вы знаете почему.
+		Иногда Винни-Пух любит вечерком во что-нибудь поиграть,  а
+	иногда,  особенно  когда  папа  дома,  он больше любит тихонько
+	посидеть у огня и послушать какую-нибудь интересную сказку.
+		В этот вечер...`
+
+	fmt.Println(Top10(str))
 }
