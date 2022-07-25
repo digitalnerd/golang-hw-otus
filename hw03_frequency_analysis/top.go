f7c0dba183269025c35e386eeb732995b3d55199
digitalnerd <levinkv.work@gmail.com>
2022-07-25 23:05:08 +0300

    Added looks like it's working!

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,47 +1,42 @@
 package main
 
 import (
-	// "fmt"
 	"fmt"
+	"sort"
 	"strings"
 )
+
 // look at this example -> https://www.geeksforgeeks.org/how-to-remove-duplicate-values-from-slice-in-golang/
-func Top10(str string) []string {
-	// fmt.Println(str)
+func Top10(str string) {
 
 	words := strings.Fields(str)
-	topWords := make(map[string]bool)
-	list := []string{}
+	topWords := make(map[string]int)
 
-	for _, entry := range words {
-		if _, value := topWords[entry]; !value {
-			topWords[entry] = true
-			list = append(list, entry)
+	for _, v := range words {
+		if _, ok := topWords[v]; !ok {
+			topWords[v] += 1
+		} else {
+			topWords[v] += 1
 		}
 	}
 
-	// fmt.Println(reflect.TypeOf(words))
+	fmt.Println(topWords)
 
-	// for i, word := range words {
-	// 	topWords[word] = i
-	// }
+	keys := make([]string, 0, len(topWords))
+	for key := range topWords {
+		keys = append(keys, key)
+	}
 
-	// for i, key := range topWords {
-	// 	fmt.Println(i, key)
-	// }
-	// fmt.Println(topWords)
+	sort.SliceStable(keys, func(i, j int) bool {
+		return topWords[keys[i]] > topWords[keys[j]]
+	})
 
-	// for i, word := range words {
-	// 	// _, ok := topWords[word]
-	// 	if _, ok := topWords[i]; ok {
-	// 		// fmt.Println(v)
-	// 	}
-	// }
-
-	return list
+	for _, k := range keys{
+        fmt.Println(k, topWords[k])
+    }
 }
 
 func main() {
-	str := "one two three one two four five six five"
-	fmt.Println(Top10(str))
+	str := "one one one one one one one one two two three four five five five six six six six six"
+	Top10(str)
 }
